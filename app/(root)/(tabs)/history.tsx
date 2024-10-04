9baecc53d27cde3f6fa159b9a499ef98fb6c03a8
[SeoHyeonSim] <[firebolt478@naver.com]>
2024-10-05 02:44:23 +0900

    'make_uber'

---
+++ app/(root)/(tabs)/history.tsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const History = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+      yaaaaaaaay
+    </SafeAreaView>
+  );
+};
+
+export default History;
